6f5d7c8100297ee5b0c8c37edb228966f69eb065
Emre Yağcı <emreygc7@gmail.com>
2022-09-01 20:09:28 +0300

    theme provider added.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import './assets/styles/global.css'
 import App from "./App";
 import WeatherContextProvider from "./context/WeatherContext";
+import ThemeContextProvider from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <WeatherContextProvider>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </WeatherContextProvider>
 );
